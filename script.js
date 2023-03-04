5fc562af88eb016aad7b472cefa9bd70c52fd792
abiola <60517156+Abbyfade@users.noreply.github.com>
2023-03-04 14:50:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+the_list = document.getElementById("list");
+
+function getApiResponse() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://swapi.dev/api/people");
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+      for (let i = 0; i < response.results.length; i++)   {
+            const main = document.createElement("div");
+            const name = document.createElement("h3");
+            const number = document.createElement("h6");
+            const po = document.createElement("img");
+            const gender = document.createElement("p")
+            const height = document.createElement("p")
+            po.src = "it.jpg";
+          
+
+            name.innerText = response.results[i].name;
+            name.className = "name"
+            gender.innerText = response.results[i].gender;
+            gender.className = "gender"
+            height.innerText = response.results[i].height;
+            height.className = "height"
+            number.innerText = i+1;
+            main.className = "info"
+            main.appendChild(number);
+            main.appendChild(po);
+            main.appendChild(name);
+            main.appendChild(gender);
+            main.appendChild(height);
+
+            the_list.appendChild(main);
+          }
+
+          function displayParagraphs(element) {
+            element.addEventListener('click', function(event) {
+              const div = element.parentNode;
+              let paragraphs = div.getElementsByTagName('p');
+          
+              for (let i = 0; i < paragraphs.length; i++) {
+              paragraphs[i].style.display = 'block';
+              }
+            });
+          }
+          
+          let paras = document.getElementsByClassName('name');
+          
+          for (let i = 0; i < paras.length; i++) {
+            displayParagraphs(paras[i]);
+          }
+
+      } else {
+        console.error(xhr.responseText);
+      }
+    };
+    xhr.send();
+  }
+
+getApiResponse()
+
+  
+  
+  
